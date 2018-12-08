8cf9eba935cf5cfc3d2be71752fcfe0bef157210
Jeremie Ges <jges@weblinc.com>
2018-12-08 11:15:56 -0800

    Refacto folio_crypto_currency update_test

--- test/operations/folio_crypto_currency/update_test.rb
+++ test/operations/folio_crypto_currency/update_test.rb
@@ -2,17 +2,32 @@ require 'test_helper'
 
 class FolioCryptoCurrencyUpdateTest < ActiveSupport::TestCase
 
-  test 'update folio crypto currency' do
-    user = UserAnonymous::Create.()['model']
-    folio = Folio::Create.({}, 'current_user' => user)['model']
-    crypto_currency = CryptoCurrency.where(symbol: 'BTC').first
-    folio_crypto_currency = FolioCryptoCurrency::Create.({crypto_currency_id: crypto_currency.id}, 'current_user' => user.reload)['model']
+  setup :user
+  setup :folio
+  setup :crypto_currency
+  setup :folio_crypto_currency
 
-    result = FolioCryptoCurrency::Update.({id: folio_crypto_currency.id, holding: 10.85}, 'current_user' => user)
-    model = result['model']
+  def user
+    @user = UserAnonymous::Create.()['model']
+  end
+
+  def folio
+    @folio = Folio::Create.({}, 'current_user' => @user)['model']
+  end
+
+  def crypto_currency
+    @crypto_currency = CryptoCurrency.where(symbol: 'BTC').first
+  end
+
+  def folio_crypto_currency
+    @folio_crypto_currency = FolioCryptoCurrency::Create.({crypto_currency_id: @crypto_currency.id}, 'current_user' => @user.reload)['model']
+  end
+
+  test 'update folio crypto currency' do
+    result = FolioCryptoCurrency::Update.({id: @folio_crypto_currency.id, holding: 10.85}, 'current_user' => user)
 
     assert result.success?
-    assert model.holding == 10.85
+    assert_equal(result['model'].holding, 10.85)
   end
 
 end
