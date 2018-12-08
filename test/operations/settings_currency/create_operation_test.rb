6ec9ef9e15cea356fd45188ddb13a7eda2134e9d
Jeremie Ges <jges@weblinc.com>
2018-12-08 12:38:58 -0800

    Refacto settings_currency/create_operation_test.rb

--- test/operations/settings_currency/create_operation_test.rb
+++ test/operations/settings_currency/create_operation_test.rb
@@ -2,34 +2,39 @@ require 'test_helper'
 
 class SettingsCurrencyCreateTest < ActiveSupport::TestCase
 
+  def user
+    @user ||= UserAnonymous::Create.()['model']
+  end
+
+  def currency_id
+    @currency_id ||= Currency.last.id
+  end
+
+  def create_folio
+    Folio::Create.({}, 'current_user' => user)
+  end
+
   test 'update currency' do
-    currency_id = Currency.last.id
-    user = UserAnonymous::Create.()['model']
-    folio = Folio::Create.({}, 'current_user' => user)
+    create_folio
 
     result = SettingsCurrency::Create.({currency_id: currency_id}, 'current_user' => user.reload)
-    model = result['model']
 
     assert result.success?
-    assert model.currency_id = currency_id
+    assert_equal(result['model'].currency_id, currency_id)
   end
 
-  test 'can\'t update if no folio' do
-    currency_id = Currency.last.id
-    user = UserAnonymous::Create.()['model']
-
+  test 'cannot update if no folio' do
     result = SettingsCurrency::Create.({currency_id: currency_id}, 'current_user' => user)
 
-    assert !result.success?
+    refute result.success?
   end
 
-  test 'can\'t update if currency does not exist' do
-    user = UserAnonymous::Create.()['model']
-    folio = Folio::Create.({}, 'current_user' => user)
+  test 'cannot update if currency does not exist' do
+    create_folio
 
     result = SettingsCurrency::Create.({currency_id: 150}, 'current_user' => user)
 
-    assert !result.success?
+    refute result.success?
   end
 
 end
